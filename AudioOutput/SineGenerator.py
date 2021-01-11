6ea5bf822de63644676ed850b59b8c22e96f9b9d
MINSOO KANG <stengine2@gmail.com>
2021-01-11 13:27:27 +0900

    Exploring Different sinewaves

---
+++ AudioOutput/SineGenerator.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_sine(frequency):
+	intervals = int(31250/frequency)
+	sinetable = np.sin(np.arange(intervals)/intervals*2*np.pi)*128+128
+	sinetable_zn = sinetable-128
+	clipping = sinetable>255
+	sinetable[clipping] = 255
+	clipping = sinetable_zn>127
+	sinetable_zn[clipping] = 127
+	sinetable = [int(i) for i in sinetable]
+	sinetable_zn = [int(i) for i in sinetable_zn]
+	return (intervals-1,sinetable,sinetable_zn)
