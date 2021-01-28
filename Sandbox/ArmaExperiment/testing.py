92e1b7945270beb634569734903a29eb2650ec5a
MINSOO KANG <stengine2@gmail.com>
2021-01-28 22:43:05 +0900

    New Wave Tables

--- Sandbox/ArmaExperiment/testing.py
+++ Sandbox/ArmaExperiment/testing.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import statsmodels.tsa.arima.model
 import numpy as np
 
-audio = (wav2float(r"D:\Dropbox\수업자료\개별연구\repo\Sandbox\ArmaExperiment\400 low.wav"))[82000:int(2.5*30875+82000)]
+audio = (wav2float(r"D:\Dropbox\수업자료\개별연구\repo\Sandbox\ArmaExperiment\1000 low.wav"))[82000:int(2.5*30875+82000)]
 audio_800 = DecreaseFrequency(list(audio),44100,800)
 audio_5000 = DecreaseFrequency(list(audio),44100,5000)
 audio_10k = DecreaseFrequency(list(audio),44100,10000)
@@ -23,17 +23,17 @@ audio_31k = DecreaseFrequency(list(audio),44100,31250)
 #res.summary()
 
 ### 25k
-# audio_25k_1byte = np.array(audio_25k)
-# audio_25k_1byte = audio_25k_1byte - min(audio_25k_1byte)
-# audio_25k_1byte = audio_25k_1byte / max(audio_25k_1byte)
-# audio_25k_1byte *= 256
-# clip = audio_25k_1byte > 255
-# audio_25k_1byte[clip_25k] = 255
-# copy_this = [int(i) for i in audio_25k_1byte]
-# print(copy_this)
-# print(len(copy_this))
-# plt.plot(copy_this)
-# plt.show()
+audio_25k_1byte = np.array(audio_25k)
+audio_25k_1byte = audio_25k_1byte - min(audio_25k_1byte)
+audio_25k_1byte = audio_25k_1byte / max(audio_25k_1byte)
+audio_25k_1byte *= 256
+clip_25k = audio_25k_1byte > 255
+audio_25k_1byte[clip_25k] = 255
+copy_this = [int(i) for i in audio_25k_1byte]
+print(copy_this)
+print(len(copy_this))
+plt.plot(copy_this)
+plt.show()
 
 ### 5k
 # audio_5k_1byte = np.array(audio_5000)
@@ -48,15 +48,16 @@ audio_31k = DecreaseFrequency(list(audio),44100,31250)
 # plt.plot(copy_this_5k)
 # plt.show()
 
-audio_10k_1byte = np.array(audio_10k)
-audio_10k_1byte = audio_10k_1byte - min(audio_10k_1byte)
-audio_10k_1byte = audio_10k_1byte / max(audio_10k_1byte)
-655360
-audio_10k_1byte *= 256
-clip_10k = audio_10k_1byte > 255
-audio_10k_1byte[clip_10k] = 255
-copy_this_10k = [int(i) for i in audio_10k_1byte]
-print(copy_this_10k)
-print(len(copy_this_10k))
-plt.plot(copy_this_10k)
-plt.show()
+###
+# audio_10k_1byte = np.array(audio_10k)
+# audio_10k_1byte = audio_10k_1byte - min(audio_10k_1byte)
+# audio_10k_1byte = audio_10k_1byte / max(audio_10k_1byte)
+# 655360
+# audio_10k_1byte *= 256
+# clip_10k = audio_10k_1byte > 255
+# audio_10k_1byte[clip_10k] = 255
+# copy_this_10k = [int(i) for i in audio_10k_1byte]
+# print(copy_this_10k)
+# print(len(copy_this_10k))
+# plt.plot(copy_this_10k)
+# plt.show()
